dc1f03c30068d042acdd81be39a06acd066e8054
jeser <jesermartha@gmail.com>
2026-03-05 21:46:26 -0600

    Corregir URL en Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const res = await axios.post('http://localhost:3000/api/auth/login', form)
+      const res = await axios.post('https://sistema-hospital-67yq.onrender.com/api/auth/login', form)
       login(res.data.token, res.data.personal)
       navigate('/')
     } catch (error) {
