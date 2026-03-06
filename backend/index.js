a1c655f297a6ceec30be2abd8f2bb036827953f9
jeser <jesermartha@gmail.com>
2026-03-05 21:58:49 -0600

    Corregir middleware express.json

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 const app = express()
 
 // Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors({
   origin: [
     'http://localhost:5173',
